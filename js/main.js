6613ff006dbdfff4114ea5d73a5e636bbb8a433a
nodes777 <nodes777@gmail.com>
2015-12-30 20:25:36 -0500

    sets model up and view 1

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ function cat(name, img, id, clicks)
    this.id = id;
    this.clicks = clicks;
 }
+
+
 var cats = [
 Carly = new cat("Carly","img/cat.jpg","cat1",0),
 Sneaker = new cat("Sneaker","img/cat2.jpg","cat2",0),
@@ -19,12 +21,26 @@ Harmon = new cat("Harmon","img/cat5.jpg", "cat5",0),
 console.log(Carly);
 console.log(cats);
 
-//octopus
+//octopus: Should start rendering
 
-//view
+
+
+//view 1: List of Cats
 var catList = document.getElementById("catList");
-var catNode = document.createElement("li");
 for(i=0; i< cats.length; i++){
-  var catList = document.createElement("li");
+  var catLi = document.createElement("p");
+  var node = document.createTextNode(cats[i].name);
+  catLi.appendChild(node);
+  catList.appendChild(catLi);
 }
 
+//View 2: Featured Cat
+function showFirstCat(cats) {
+  var featuredCat = document.getElementById("featuredCat")
+  var catPic = document.createElement("img");
+  catPic.setAttribute('src', cats[0].img);
+  catPic.setAttribute("width", "400");
+  catPic.setAttribute("height", "300");
+  featuredCat.appendChild(catPic);
+}
+showFirstCat();
